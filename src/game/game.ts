037fd9dceaa51eec34363cd073519b7c7576c69d
bendarsiayoub <bendarsiayoub@gmail.com>
2023-12-22 10:16:25 +0100

    Refactor: removed gameChatClient and socket from game class to make it more reusable

--- src/game/game.ts
+++ src/game/game.ts
@@ -2,61 +2,28 @@ import getRandomInterval from './utils/randomInterval';
 import { generateWord } from './utils/wordGenerator';
 import redisClient from '@/database/redisClient';
 import adjustDifficulty from './difficulty';
-import logger from '@/utils/logger';
-import { gameChatClient } from '@/twitch/chatClients';
-import { Socket } from 'socket.io';
+import { EventEmitter } from 'events';
 
 class Game {
     private wordsTimeouts = new Map<string, NodeJS.Timeout>();
     private running: boolean = false;
     private wordGenerateTimoutId: NodeJS.Timeout | null = null;
 
-    // in game related properties
-    private score: number = 0;
+    public score: number = 0;
     private difficulty = adjustDifficulty(this.score);
+    public channelUsername: string | undefined;
+    public eventEmitter: EventEmitter = new EventEmitter();
 
-    constructor(
-        private channelUsername: string,
-        private socket: Socket,
-    ) {
-        this.createNewGame(channelUsername);
-        logger.info(`Joined ${channelUsername}`);
-    }
-
-    private async createNewGame(channelUsername: string) {
-        try {
-            await gameChatClient.join(channelUsername);
-            gameChatClient.onMessage((channel, user, message, msg) => {
-                // checks if the message is coming from the channel that the game was created on,
-                if (msg.userInfo.isBroadcaster && channel === channelUsername) {
-                    if (message == '!start' && !this.running) {
-                        this.startGame(channelUsername);
-                    }
-                    if (message == '!stop' && this.running) {
-                        this.stopGame(channelUsername);
-                    }
-                }
-            });
-        } catch (error) {
-            logger.error(error);
-        }
-    }
-
-    private startGame(channelUsername: string) {
-        logger.info(`game started on ${channelUsername}`);
-        this.socket.emit('gameStatus', { channelUsername, status: 'Game started' });
+    public startGame(channelUsername: string) {
+        this.channelUsername = channelUsername;
         this.running = true;
         this.runGameLoop(channelUsername);
-        this.checkAndRemoveMatchedWords();
+        return `Game started successfully on ${channelUsername}!`;
     }
 
-    private async stopGame(channel: string, gameOver?: boolean) {
-        if (gameOver) {
-            logger.info(`game over on ${channel}`);
-            this.socket.emit('gameStatus', { channel, status: 'Game over' });
-        } else {
-            logger.info(`game stopped on ${channel}`);
-            this.socket.emit('gameStatus', { channel, status: 'Game stopped' });
+    public async stopGame(channel: string, gameOver?: boolean) {
+        if (!this.running) {
+            return "Game isn't running";
         }
         this.running = false;
         this.score = 0;
@@ -74,61 +41,51 @@ class Game {
             clearTimeout(timeout);
         });
         this.wordsTimeouts.clear();
+        return gameOver ? 'Game over' : 'Game stopped successfully';
     }
 
     private runGameLoop(channel: string) {
         const { wordMinLength, wordMaxLength, wordTimeout, wordInterval } = this.difficulty;
-        if (this.running) {
-            this.wordGenerateTimoutId = setTimeout(
-                async () => {
-                    const word = generateWord({
-                        minLength: wordMinLength,
-                        maxLength: wordMaxLength,
-                    });
-                    logger.info(word);
-                    this.socket.emit('word', { word: word });
-                    const myObj = {
-                        word: word,
-                        difficulty: adjustDifficulty(this.score),
-                    };
-                    logger.info(myObj);
-                    await redisClient.SADD(`words:${channel}`, word);
 
-                    this.wordsTimeouts.set(
-                        word,
-                        setTimeout(() => {
-                            logger.info(`Timeout for word: ${word}`);
-                            this.socket.emit('wordTimeout', { word: word });
-                            this.stopGame(channel, true);
-                        }, wordTimeout),
-                    );
+        this.wordGenerateTimoutId = setTimeout(
+            async () => {
+                const word = generateWord({
+                    minLength: wordMinLength,
+                    maxLength: wordMaxLength,
+                });
 
-                    this.runGameLoop(channel);
-                },
-                getRandomInterval(wordInterval[0], wordInterval[1]),
-            );
-        }
+                await redisClient.SADD(`words:${channel}`, word);
+
+                this.eventEmitter.emit('newWord', word);
+
+                this.wordsTimeouts.set(
+                    word,
+                    setTimeout(() => {
+                        this.eventEmitter.emit('wordTimout', word);
+                        this.stopGame(channel, true);
+                    }, wordTimeout),
+                );
+
+                this.runGameLoop(channel);
+            },
+            getRandomInterval(wordInterval[0], wordInterval[1]),
+        );
     }
 
-    private checkAndRemoveMatchedWords() {
-        gameChatClient.onMessage(async (channel, user, message) => {
-            if (!this.running) {
-                return;
-            }
-            const wordExist = await redisClient.SISMEMBER(`words:${this.channelUsername}`, message);
-            if (wordExist) {
-                const timeout = this.wordsTimeouts.get(message);
-                if (timeout) {
-                    clearTimeout(timeout);
-                    this.wordsTimeouts.delete(message);
-                }
+    async removeMatchedWords(word: string) {
+        if (!this.running) {
+            return;
+        }
+        const wordExist = await redisClient.SISMEMBER(`words:${this.channelUsername}`, word);
+        if (wordExist) {
+            const timeout = this.wordsTimeouts.get(word);
+            clearTimeout(timeout);
+            this.wordsTimeouts.delete(word);
 
-                await redisClient.SREM(`words:${this.channelUsername}`, message);
-                this.score += 1;
-                logger.info(`score: ${this.score}`);
-                this.socket.emit('score', { score: this.score });
-            }
-        });
+            await redisClient.SREM(`words:${this.channelUsername}`, word);
+            this.score += 1;
+            return word;
+        }
     }
 }
 
--- src/socket/events/gameOverlay.ts
+++ src/socket/events/gameOverlay.ts
@@ -1,7 +1,6 @@
 import { Socket } from 'socket.io';
 import redisClient from '@/database/redisClient';
 import Broadcaster from '@/database/models/broadcaster';
-import logger from '@/utils/logger';
 import Game from '@/game/game';
 import { gameChatClient } from '@/twitch/chatClients';
 
@@ -13,21 +12,17 @@ export async function handleGameOverlay(socket: Socket) {
     if (session) {
         socket.emit('error', { error: 'session already exist' });
         socket.disconnect();
-        logger.error(`Session ${sessionId} already exist`);
     } else {
         const broadcaster = await Broadcaster.findOne({ sessionId }).exec();
 
         if (broadcaster) {
             await redisClient.HSET('sessions', sessionId, broadcaster.username);
             socket.emit('success', { message: 'session created' });
-            new Game(broadcaster.username, socket);
-
-            logger.info(`Session ${sessionId} for ${broadcaster.username} created`);
+            new Game();
 
             socket.on('disconnect', async () => {
                 gameChatClient.part(broadcaster.username);
                 await redisClient.HDEL('sessions', sessionId);
-                logger.info(`Session ${sessionId} deleted`);
             });
         } else {
             socket.emit('error', { error: 'broadcaster not found' });
