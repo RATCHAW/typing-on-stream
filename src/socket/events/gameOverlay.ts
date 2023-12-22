aacd41de2986467373fd36934452287a31d7f25e
bendarsiayoub <bendarsiayoub@gmail.com>
2023-12-22 14:18:37 +0100

    feat: Add emit events

--- src/socket/events/gameOverlay.ts
+++ src/socket/events/gameOverlay.ts
@@ -1,8 +1,9 @@
 import { Socket } from 'socket.io';
 import redisClient from '@/database/redisClient';
 import Broadcaster from '@/database/models/broadcaster';
+// import { gameChatClient } from '@/twitch/chatClients';
 import Game from '@/game/game';
-import { gameChatClient } from '@/twitch/chatClients';
+import { ChatClient } from '@twurple/chat';
 
 export async function handleGameOverlay(socket: Socket) {
     const workspace = socket.nsp.name;
@@ -16,12 +17,43 @@ export async function handleGameOverlay(socket: Socket) {
         const broadcaster = await Broadcaster.findOne({ sessionId }).exec();
 
         if (broadcaster) {
-            await redisClient.HSET('sessions', sessionId, broadcaster.username);
-            socket.emit('success', { message: 'session created' });
-            new Game();
+            const game = new Game();
+            const gameChatClient = new ChatClient({ channels: [broadcaster.username] });
+            gameChatClient.connect();
+            gameChatClient.onConnect(async () => {
+                await redisClient.HSET('sessions', sessionId, broadcaster.username);
+                socket.emit('success', { message: 'session created' });
+            });
+
+            gameChatClient.onMessage(async (channel, user, message, msg) => {
+                const destroyedWord = await game.removeMatchedWords(message);
+
+                if (destroyedWord) {
+                    socket.emit('destroyedWord', { destroyedWord });
+                }
+
+                if (msg.userInfo.isBroadcaster && channel === broadcaster.username) {
+                    if (message == '!start') {
+                        const status = game.startGame(broadcaster.username);
+                        socket.emit('gameStatus', { status });
+                    }
+                    if (message == '!stop') {
+                        const status = await game.stopGame();
+                        socket.emit('gameStatus', { status });
+                    }
+                }
+            });
+
+            game.eventEmitter.on('newWord', (word) => {
+                socket.emit('newWord', { word });
+            });
+
+            game.eventEmitter.on('gameOver', (status, word) => {
+                socket.emit('gameStatus', { status, word });
+            });
 
             socket.on('disconnect', async () => {
-                gameChatClient.part(broadcaster.username);
+                gameChatClient.quit();
                 await redisClient.HDEL('sessions', sessionId);
             });
         } else {
