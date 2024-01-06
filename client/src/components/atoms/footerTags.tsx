1697a5b8fa169c7a96f4d7d29ece58c27e15ce88
RATCHAW <bendarsiayoub@gmail.com>
2024-01-06 05:27:52 +0100

    chore(ui): better syntax

--- client/src/components/atoms/footerTags.tsx
+++ client/src/components/atoms/footerTags.tsx
@@ -1,14 +1,33 @@
 import ratTyping from '/emotes/ratTyping.gif';
 import peepoFigma from '/emotes/peepoFigma.png';
 
+function Ekb() {
+  return (
+    <div className="flex items-center space-x-2 mx-2">
+      <img width={29} height={29} src={peepoFigma} alt="ratchaw" />
+      <a href="#" className="text-[#FAC337]">
+        EKB9816
+      </a>
+    </div>
+  );
+}
+
+function Ratchaw() {
+  return (
+    <div className="flex items-center space-x-2 mx-2">
+      <img width={29} height={29} src={ratTyping} alt="ekb" />
+      <a href="#" className="text-[#FAC337]">
+        RATCHAW
+      </a>
+    </div>
+  );
+}
+
 function FooterTag() {
   return (
-    <p className="flex items-center  text-[#767676] text-[8px]">
-      made with love by <img className="w-[29px] h-[29px] mx-2" src={ratTyping} alt="" />
-      <span className="text-[#FAC337]">ratchaw&nbsp; </span>&
-      <img className="w-[29px] h-[29px] mx-2" src={peepoFigma} alt="" />
-      <span className="text-[#FAC337]">ekb9816</span>
-    </p>
+    <div className="flex items-center  text-[#767676] text-[8px]">
+      Made with love by <Ratchaw /> & <Ekb />
+    </div>
   );
 }
 
